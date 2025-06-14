b69f47b8d0c50bc00043fa0d7e5ffe1fc1cad31b
Gbenga <gbengaemmanuel450@gmail.com>
2025-06-14 11:22:25 +0100

    Correct the unittest script

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,11 +8,12 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-5, -5), -10)
         self.assertEqual(self.calc.add(0, 0), 0)
+
 
     def test_subtract(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
